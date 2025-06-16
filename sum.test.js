f3e537d72f8018c2122aa314ab89d7d8f4167d3e
lucascrvz <lukassforce123@gmail.com>
2025-06-16 12:25:52 -0400

    creacion de test para funcion sum

--- sum.test.js
+++ sum.test.js
@@ -2,4 +2,8 @@ const sum = require('./sum')
 
 test('Suma dos números', () => {
     expect(sum(1, 2)).toBe(3);
+    expect(sum(-1, 1)).toBe(0);
+    expect(sum(0, 0)).toBe(0);
+    expect(sum(100, 200)).toBe(300);
+    expect(sum(-100, -200)).toBe(-300);
 });
